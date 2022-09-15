087a790532bde20d4de61f0362577a2947954b8c
LeonardoMonti <LeonardoMonti>
2022-09-15 20:22:03 -0300

    feat: add Interface for valid ID car

---
+++ src/interfaces/ICarId.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const idCarZodSchema = z.object({
+  id: z.string({
+    required_error: 'Id is required',
+    invalid_type_error: 'Id must be a string',
+  })
+    .length(24, { message: 'Id must have 24 hexadecimal characters' }),
+});
+
+type ID = z.infer<typeof idCarZodSchema>;
+
+export { ID, idCarZodSchema };
