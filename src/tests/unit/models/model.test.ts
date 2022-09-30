35ed4c5118e3ee082d0075cff0c2d1f90eadf8ea
LeonardoMonti <LeonardoMonti>
2022-09-30 06:46:35 -0300

    test: model motorcycle

--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -5,7 +5,11 @@ import { expect } from 'chai';
 import CarModel from '../../../models/Car';
 import { ICar } from '../../../interfaces/ICar';
 
-import { idValid, validCar, validCarUpdated } from '../../utils/constants';
+import { idValid, validCar, validCarUpdated, validMotorcycle, validMotorcycleUpdated } from '../../utils/constants';
+
+import MotorcycleModel from '../../../models/Motorcycle';
+import { IMotorcycle } from '../../../interfaces/IMotorcycle';
+
 
 describe('Testing Model', () => {
   beforeEach(() => sinon.restore())
@@ -85,6 +89,88 @@ describe('Testing Model', () => {
   
     it('5.1 test function delete', () => {
       expect(resultCar).to.have.property('_id');
+    });
+  });
+})
+
+describe('Testing Model', () => {
+  beforeEach(() => sinon.restore())
+  
+  describe('1- Must create a new motorcycle', () => {
+    let resultMotorcycle: IMotorcycle | null;
+    
+    before(async () => {
+      const motorcycleModel = new MotorcycleModel();
+      sinon.stub(mongoose.Model, 'create').resolves(validMotorcycle);
+  
+      resultMotorcycle = await motorcycleModel.create(validMotorcycle);
+    });
+  
+    it('1.1 test creation function', async () => {
+      expect(resultMotorcycle).to.have.property('_id');
+    });
+  });
+
+  describe('2- Must consult all registered motorcycles', () => {
+    let resultMotorcycle: IMotorcycle[] | null;
+  
+    before(async () => {
+      const motorcycleModel = new MotorcycleModel();
+      sinon.stub(mongoose.Model, 'find').resolves([validMotorcycle]);
+  
+      resultMotorcycle = await motorcycleModel.read();
+    });
+  
+    it('2.1 test read function', () => {
+      expect(resultMotorcycle).to.be.an('array');
+      expect(resultMotorcycle).to.have.lengthOf(1);
+    });
+  });
+
+  describe('3- Must consult one motorcycle on database using valid ID', () => {
+    let resultMotorcycle: IMotorcycle | null;
+    
+    before(async () => {
+      const motorcycleModel = new MotorcycleModel();
+      sinon.stub(mongoose.Model, 'findOne').resolves(validMotorcycle);
+
+      resultMotorcycle = await motorcycleModel.readOne(idValid);
+    });
+  
+    it('3.1 test readOne function', () => {
+      expect(resultMotorcycle).to.have.property('_id');
+    });
+  });
+
+  describe('4- Must change/update one motorcycle on database', () => {
+    let resultMotorcycle: IMotorcycle | null;
+  
+    before(async () => {
+      const motorcycleModel = new MotorcycleModel();
+      sinon.stub(mongoose.Model, 'findOneAndUpdate').resolves(validMotorcycleUpdated);
+      
+      resultMotorcycle = await motorcycleModel.update(idValid, validMotorcycleUpdated);
+    });
+  
+    it('4.1 test function update', () => {
+      expect(resultMotorcycle?.model).to.equals('Honda CB 1000');
+      expect(resultMotorcycle?.year).to.equals(2022);
+      expect(resultMotorcycle?.color).to.equals('white');
+    });
+  });
+
+  describe('5- Must remove/delete one motorcycle on database', () => {
+    let resultMotorcycle: IMotorcycle | null;
+  
+    before(async () => {
+      const motorcycleModel = new MotorcycleModel();
+      sinon.stub(mongoose.Model, 'findOneAndDelete').resolves(validMotorcycle);
+      
+      resultMotorcycle = await motorcycleModel.delete(idValid);
+    });
+  
+    it('5.1 test function delete', () => {
+      expect(resultMotorcycle).to.have.property('_id');
     });
   });
 })
