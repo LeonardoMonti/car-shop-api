8e8dd39ab616e4bb83004f9a96ef7036b61ee847
LeonardoMonti <LeonardoMonti>
2022-09-30 06:48:53 -0300

    fix: add constants motorcycle

--- src/tests/utils/constants.ts
+++ src/tests/utils/constants.ts
@@ -1,4 +1,5 @@
 import { ICar } from "../../interfaces/ICar";
+import { IMotorcycle } from "../../interfaces/IMotorcycle";
 
 export const idValid = 'aB1cD2eF3gH4iJ5kL6mN7oP8';
 
@@ -20,4 +21,24 @@ export const validCarUpdated: ICar & { _id: string } = {
   buyValue: 12000,
   doorsQty: 4,
   seatsQty: 2,
+};
+
+export const validMotorcycle: IMotorcycle & { _id: string }= {
+  _id: idValid,
+  model: 'Honda CB 600',
+  year: 2020,
+  color: 'black',
+  buyValue: 19000,
+  category: 'Street',
+  engineCapacity: 2000
+};
+
+export const validMotorcycleUpdated: IMotorcycle & { _id: string } = {
+  _id: idValid,
+  model: 'Honda CB 1000',
+  year: 2022,
+  color: 'white',
+  buyValue: 50000,
+  category: 'Street',
+  engineCapacity: 2499
 };
