02c6c04b53582573b069142033bbe35be14378e0
LeonardoMonti <LeonardoMonti>
2022-09-30 05:53:33 -0300

    fix: add route motorcycle in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
-import carRouter from './routes/CarRoute';
+import { CarRoute, MotorcyleRoute } from './routes';
 
 const app = express();
 app.use(express.json());
-app.use(carRouter);
+
+app.use(CarRoute);
+app.use(MotorcyleRoute);
 
 export default app;
