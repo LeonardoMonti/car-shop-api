ce405f00136bf7d7024c262c07926d81276c4d27
LeonardoMonti <LeonardoMonti>
2022-09-30 06:46:53 -0300

    test: service motorcycle

--- src/tests/unit/services/service.test.ts
+++ src/tests/unit/services/service.test.ts
@@ -6,7 +6,10 @@ import { ICar } from '../../../interfaces/ICar';
 import { ServiceError } from '../../../services';
 import CarService from '../../../services/CarService';
 
-import { idValid, validCar, validCarUpdated } from '../../utils/constants';
+import { IMotorcycle } from '../../../interfaces/IMotorcycle';
+import MotorcyleService from '../../../services/MotorcyleService';
+
+import { idValid, validCar, validCarUpdated, validMotorcycle, validMotorcycleUpdated } from '../../utils/constants';
 
 describe('Testing Service', () => {
   beforeEach(() => sinon.restore())
@@ -85,6 +88,86 @@ describe('Testing Service', () => {
     });
   });
 })
+
+
+describe('Testing Service', () => {
+  beforeEach(() => sinon.restore())
+
+  describe('1- Must create a new Motorcyle', () => {
+    let resultCar: IMotorcycle | ServiceError | null;
+    
+    it('1.1 Test create Motorcyle function on Service passing data correctly', async () => {
+      const motorcycleService = new MotorcyleService();
+      sinon.stub(mongoose.Model, 'create').resolves(validMotorcycle);
+  
+      resultCar = await motorcycleService.create(validMotorcycle);
+
+      expect(resultCar).to.have.property('model');
+    });
+  });
+
+  describe('2- Must consult all registered Motorcyles', () => {
+    let resultCar: IMotorcycle[] | null;
+  
+    before(async () => {
+      const motorcycleService = new MotorcyleService();
+      sinon.stub(mongoose.Model, 'find').resolves([validMotorcycle]);
+
+      resultCar = await motorcycleService.read();
+    });
+  
+    it('2.1 Test function on Service list all Motorcyles', () => {
+      expect(resultCar).to.be.an('array');
+      expect(resultCar).to.have.lengthOf(1);
+    });
+  });
+
+  describe('3 - Must consult one Motorcyle on database using valid ID', () => {
+    let resultCar: IMotorcycle | ServiceError | null;
+  
+    before(async () => {
+      const motorcycleService = new MotorcyleService();
+      sinon.stub(mongoose.Model, 'findOne').resolves(validMotorcycle);
+
+      resultCar = await motorcycleService.readOne(idValid);
+    });
+  
+    it('3.1 Test function on Service get one Motorcyle valid ID', () => {
+      expect(resultCar).to.have.property('_id');
+    });
+  });
+
+  describe('4- Must change/update one Motorcyle on database', () => {
+    let resultCar: IMotorcycle | ServiceError | null;
+  
+    before(async () => {
+      const motorcycleService = new MotorcyleService();
+      sinon.stub(mongoose.Model, 'findOneAndUpdate').resolves(validMotorcycleUpdated);
+  
+      resultCar = await motorcycleService.update(idValid, validMotorcycle);
+    });
+  
+    it('4.1 Test function on Service update one Motorcyle using valid ID', () => {
+      expect(resultCar).to.have.property('_id');
+    });
+  });
+  
+  describe('5- Must remove/delete one Motorcyle on database', () => {
+    let resultCar: IMotorcycle | ServiceError | null;
+  
+    before(async () => {
+      const motorcycleService = new MotorcyleService();
+      sinon.stub(mongoose.Model, 'findOneAndDelete').resolves(validMotorcycle);
+  
+      resultCar = await motorcycleService.delete(idValid);
+    });
+  
+    it('5.1 Test function on Service delete one Motorcyle using Valid ID', () => {
+      expect(resultCar).to.have.property('_id');
+    });
+  });
+})
+
 
 
 
