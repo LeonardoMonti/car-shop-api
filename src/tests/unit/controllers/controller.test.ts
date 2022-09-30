396dbd0308c3edf836a4ee5ce9a4558b5b1e8218
LeonardoMonti <LeonardoMonti>
2022-09-30 06:47:05 -0300

    test: controller motorcycle

--- src/tests/unit/controllers/controller.test.ts
+++ src/tests/unit/controllers/controller.test.ts
@@ -5,7 +5,7 @@ import chaiHttp from 'chai-http';
 
 import App from '../../../app';
 
-import { idValid, validCar, validCarUpdated } from '../../utils/constants';
+import { idValid, validCar, validCarUpdated, validMotorcycle, validMotorcycleUpdated} from '../../utils/constants';
 
 chai.use(chaiHttp);
 
@@ -78,6 +78,81 @@ describe('Testing Controller', () => {
     it('5.1 tests the Controller delete function by informing the correct ID', async () => {
       const chaiHttpResponse = await chai.request(App)
         .delete(`/cars/${idValid}`);
+  
+      expect(chaiHttpResponse).to.have.status(204);
+    });
+  });
+})
+
+describe('Testing Controller', () => {
+  before(() => {
+    sinon.stub(mongoose.Model, 'create').resolves(validMotorcycle as any);
+    sinon.stub(mongoose.Model, 'find').resolves([validMotorcycle] as any);
+    sinon.stub(mongoose.Model, 'findOne').resolves(validMotorcycle as any);
+    sinon.stub(mongoose.Model, 'findOneAndUpdate').resolves(validMotorcycleUpdated as any);
+    sinon.stub(mongoose.Model, 'findOneAndDelete').resolves(validMotorcycle as any);
+  })
+
+  after(() => sinon.restore())
+
+  describe('1- When entering a new Motorcycle', () => {
+    it('1.1 tests the Controller create function by entering all the data correctly', async () => {
+      const chaiHttpResponse = await chai.request(App)
+        .post('/motorcycles').send(validMotorcycle);
+  
+      expect(chaiHttpResponse).to.have.status(201);
+    });
+  });
+
+  describe('2- Query all motorcycles registered in the database', () => {
+    // before(() => {
+    //   sinon.stub(mongoose.Model, 'find').resolves([validMotorcycle] as any);
+    // });
+  
+    it('2.1 tests the Controller read function by informing all the data correctly', async () => {
+      const chaiHttpResponse = await chai.request(App)
+        .get('/motorcycles');
+  
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+    });
+  });
+
+  describe('3- Query a Motorcycle in the database', () => {
+    // before(() => {
+    //   sinon.stub(mongoose.Model, 'findOne').resolves(validMotorcycle as any);
+    // });
+  
+    it('3.1 tests the Controller readOne function by informing the correct ID', async () => {
+      const chaiHttpResponse = await chai.request(App)
+        .get(`/motorcycles/${idValid}`);
+  
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.have.property('_id');
+    });
+  });
+
+  describe('Altera um carro no banco de dados', () => {
+    // before(() => {
+    //   sinon.stub(mongoose.Model, 'findOneAndUpdate').resolves(validCarUpdated as any);
+    // });
+  
+    it('4- tests the Controller update function by informing all the data correctly', async () => {
+      const chaiHttpResponse = await chai.request(App)
+        .put(`/motorcycles/${idValid}`)
+        .send(validMotorcycle);
+        
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body.model).to.be.equal('Honda CB 1000');
+      expect(chaiHttpResponse.body.year).to.be.equal(2022);
+      expect(chaiHttpResponse.body.color).to.be.equal('white');
+    });
+  });
+
+  describe('5- Remove a Motorcycle from the database', () => {
+    it('5.1 tests the Controller delete function by informing the correct ID', async () => {
+      const chaiHttpResponse = await chai.request(App)
+        .delete(`/motorcycles/${idValid}`);
   
       expect(chaiHttpResponse).to.have.status(204);
     });
